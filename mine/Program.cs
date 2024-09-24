9846e02f5c4dcf4d6bebc6fd0b4baaffb1bb83e2
blob <87500152+Todor1000@users.noreply.github.com>
2024-09-24 17:53:51 +0200

    Add files via upload

---
+++ mine/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+   string[] inputTrucks= Console.ReadLine().Split(";").ToArray();
+            List<Truck> listOfTrucks = new List<Truck>();
+            foreach(string item in inputTrucks)
+            {
+                string[] trucks = Console.ReadLine().Split("=").ToArray();
+                listOfTrucks.Add(new Truck(trucks[0], double.Parse(trucks[1])));
+            }
+        }
+    }
+}
